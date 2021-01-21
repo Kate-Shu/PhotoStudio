bb2361cdcd620f05a83f51012a4a975cd08b198a
Kateryna Onufrichuk <katerynaonufrichuk@Katerynas-MacBook-Pro.local>
2021-01-21 18:18:37 +0400

    add transition for photo

--- js003.js
+++ js003.js
@@ -1,8 +1,45 @@
+//appearing title
 const rowImgContainer = document.querySelector('.row-img-container');
-const videoItemSection = document.querySelector('.video-item-section')
-
-
-
 setTimeout(() => rowImgContainer.classList.add('active'), 1000);
 
-const appearOnScroll = new IntersectionObserver (function(entries, appearOnScroll) {}, options);
+//appearing photo
+const faderPhoto = document.querySelectorAll('.column');
+const appearPtotoOptions = {
+ threshold: 0.5,
+ rootMargin: '0px 0px -100px 0px'
+};
+const appearPhotoOnScroll = new IntersectionObserver (function(entries, appearPhotoOnScroll){
+ entries.forEach(entry => {
+  if(!entry.isIntersecting){
+   return;
+  }else{
+   entry.target.classList.add('appear');
+   appearPhotoOnScroll.unobserve(entry.target);
+  }
+ });
+}, appearPtotoOptions);
+faderPhoto.forEach(fader => {
+ appearPhotoOnScroll.observe(fader);
+})
+
+//appearing video files
+const faderVideo = document.querySelectorAll('.video-item-section');
+const appearVideoOptions = {
+ threshold: 0.5,
+ rootMargin: '0px 0px -100px 0px'
+};
+
+const appearVideoOnScroll = new IntersectionObserver (function(entries, appearVideoOnScroll) {
+ entries.forEach(entry => {
+  if(!entry.isIntersecting){
+   return;
+  }else{
+   entry.target.classList.add('appear');
+   appearVideoOnScroll.unobserve(entry.target);
+  }
+ });
+}, appearVideoOptions);
+
+faderVideo.forEach(fader => {
+ appearVideoOnScroll.observe(fader);
+});
